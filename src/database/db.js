13db5348f20d33b479fb000f9788d87042a0255f
Maristina-Hub <maristyna@yahoo.com>
2021-10-09 06:46:19 +0100

    first commit

---
+++ src/database/db.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+const mongoServer = await MongoMemoryServer.create();
+
+const dbConnection = {
+    getConnect:async () => {
+        try{
+            await mongoose.connect(mongoServer.getUri(), { 
+                useNewUrlParser: true, 
+                dbName: "verifyMASTER", 
+                useCreateIndex: true, 
+                useUnifiedTopology: true });
+
+                // On success
+                console.log('Database connected successfully')
+
+            await mongoose.disconnect();
+        } catch(err) {
+            console.error(err.message);
+            process.exit(1);
+            }
+
+    }
+};
+
+
+export default dbConnection
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes/router.js';
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Just for testing
+app.get('/', (req, res) => {
+    res.json({
+    status: 'success',
+    message: 'Welcome to our Book-lending app.',
+    });
+});
+
+app.use(router);
+
+
+export default app;
