304a67ed439e06320a4a51c51692800b8d213845
Toheeb <toheebyekeen@gmail.com>
2021-10-15 08:31:28 +0100

    feature update

--- src/database/test_db.js
+++ src/database/test_db.js
@@ -1,34 +1,26 @@
-// import mongoose from 'mongoose';
-// import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
-// // ======= CONFIGURING MONGO MEMORY SERVER ========
-// const mongoServer = new MongoMemoryServer();
+const mongoServer = new MongoMemoryServer();
 
-// // Connect to db
-// export const connect = async () => {
-// const URI = await mongoServer.getUri();
+// Connect to db
+export const connect = async () => {
+  const URI = await mongoServer.getUri();
 
-// await mongoose.connect(URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-// })
-// }
+  // Disconnect from db and close connection
+  export const disconnectDB = async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongoServer.stop();
+  };
 
-// // Disconnect from db and close connection
-// export const disconnectDB = async () => {
-//     await mongoose.connection.dropDatabase();
-//     await mongoose.connection.close();
-//     await mongoServer.stop();
-// }
+  // Cleat the db, and remove all data
+  export const clearDatabase = async () => {
+    const collections = mongoose.connection.collections;
 
-// // Cleat the db, and remove all data
-// export const clearDatabase = async () => {
-//     const collections = mongoose.connection.collections;
-
-//     for(const key in collections) {
-//         const collection = collections[key];
-//         await collection.deleteMany();
-//     }
-//     }
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany();
+    }
+  };
+};
