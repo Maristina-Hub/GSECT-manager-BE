297b976f136eb5aff55c6cc9ee3f0cff4ebe3e10
unknown <maristyna@yahoo.com>
2021-10-17 22:26:34 +0100

    heroku update

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+//const uri = 'mongodb+srv://test:test@123@cluster0.w8yce.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 export const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
