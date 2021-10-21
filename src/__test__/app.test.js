8c1e43404f097552a0426d0f25f07dc2940f9f70
Toheeb <toheebyekeen@gmail.com>
2021-10-21 12:24:12 +0100

    fixed test error

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -3,15 +3,8 @@ import app from "../server.js";
 
 const request = supertest(app);
 
-<<<<<<< HEAD
 it("get route", async () => {
   const response = await request.get("/");
   expect(200);
   expect(response.body.message).toBe("Welcome to our GSECT-MAMAGER App.");
-=======
-it('get route', async () => {
-    const response = await request.get('/');
-    expect(200);
-    expect(response.body.message).toBe('Welcome to our GSECT-MAMAGER App.');
->>>>>>> refs/remotes/origin/feature
 });
