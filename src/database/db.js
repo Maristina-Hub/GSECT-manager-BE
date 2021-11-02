e950a4819cb65889fbfcdd136f7b4a21af5271ae
unknown <maristyna@yahoo.com>
2021-11-02 21:32:40 +0100

    updated db

--- src/database/db.js
+++ src/database/db.js
@@ -5,9 +5,9 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useCreateIndex: false,
+    useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
   });
 
   console.log("Database connected successfully.");
