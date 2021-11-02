b1b28fd617ffb011fc56709c433afdec336350ac
unknown <maristyna@yahoo.com>
2021-11-02 20:36:55 +0100

    commit for deploy to heroku

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI, {
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
-// import router from "./routes/router.js";
-import router from "./routes/router.js";
+import userRoutes from "./routes/userRoutes.js";
 import privateRoute from "./routes/privateRoute.js";
 import { errorHandler } from "./middleware/error.js";
 
