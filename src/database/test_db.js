9c783965f2c349a5c753ef37d6482b85d0cdab2e
unknown <maristyna@yahoo.com>
2021-11-05 11:41:20 +0100

    updated sub controller

--- src/database/test_db.js
+++ src/database/test_db.js
@@ -1,28 +1,28 @@
 
-import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
+// import mongoose from 'mongoose';
+// import { MongoMemoryServer } from 'mongodb-memory-server';
 
-const mongoServer = new MongoMemoryServer()
+// const mongoServer = new MongoMemoryServer()
 
-// Connect to db
-export const connect = async () => {
-const URI = await mongoServer.getUri()
+// // Connect to db
+// export const connect = async () => {
+// const URI = await mongoServer.getUri()
 
-// Disconnect from db and close connection
-export const disconnectDB = async () => {
-    await mongoose.connection.dropDatabase();
-    await mongoose.connection.close();
-    await mongoServer.stop();
-}
+// // Disconnect from db and close connection
+// export const disconnectDB = async () => {
+//     await mongoose.connection.dropDatabase();
+//     await mongoose.connection.close();
+//     await mongoServer.stop();
+// }
 
-// Cleat the db, and remove all data
-export const clearDatabase = async () => {
-    const collections = mongoose.connection.collections;
+// // Cleat the db, and remove all data
+// export const clearDatabase = async () => {
+//     const collections = mongoose.connection.collections;
 
-    for(const key in collections) {
-        const collection = collections[key];
-        await collection.deleteMany();
-    }
-}
-}
+//     for(const key in collections) {
+//         const collection = collections[key];
+//         await collection.deleteMany();
+//     }
+// }
+// }
 
